d06b2d9ced79af3f56d17e65b36f8bfc1a5f23ea
Diogo Santana <126726702+diogosfc@users.noreply.github.com>
2023-08-30 21:03:35 -0300

    ex2.6.b.di.ts

---
+++ ex2.6.b.di.ts
@@ -0,0 +1,13 @@
+enum DiasDaSemana {
+    Domingo = 1,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado,
+}
+
+console.log(DiasDaSemana.Segunda);
+console.log(DiasDaSemana.Quarta);
+console.log(DiasDaSemana.Sabado);
